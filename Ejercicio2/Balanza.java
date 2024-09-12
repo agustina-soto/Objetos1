26ffecd9527fc6c6ab3cd167816f8886c9cffbaf
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2024-09-12 15:01:22 -0300

    Create Balanza.java

---
+++ Ejercicio2/Balanza.java
@@ -0,0 +1,51 @@
+package ar.edu.unlp.info.oo1.balanzaElectronica;
+
+public class Balanza {
+	private int cantidadDeProductos;
+	private double precioTotal;
+	private double pesoTotal;
+	
+	public Balanza() {
+		
+	}
+	
+	public int getCantidadDeProductos() {
+		return this.cantidadDeProductos;
+	}
+	
+	public void setCantidadDeProductos(int unaCantidad) {
+		this.cantidadDeProductos = unaCantidad;
+	}
+	
+	public double getPrecioTotal() {
+		return this.precioTotal;
+	}
+	
+	public void setPrecioTotal(double unPrecioTotal) {
+		this.precioTotal = unPrecioTotal;
+	}
+
+	public double getPesoTotal() {
+		return this.pesoTotal;
+	}
+	
+	public void setPesoTotal(double unPesoTotal) {
+		this.pesoTotal = unPesoTotal;
+	}
+
+	public void ponerEnCero() {
+		this.cantidadDeProductos = 0;
+		this.precioTotal = 0;
+		this.pesoTotal = 0;
+	}
+	
+	public void agregarProducto(Producto producto) {
+		this.cantidadDeProductos++;
+		this.precioTotal += producto.getPrecio();
+		this.pesoTotal += producto.getPeso();
+	}
+	
+	public Ticket emitirTicket() {
+		return new Ticket(this.cantidadDeProductos, this.precioTotal, this.pesoTotal);
+	}
+}
