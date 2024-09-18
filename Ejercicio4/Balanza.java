630020db8c71a38d2e5bb242c5618d0501678412
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2024-09-18 17:53:06 -0300

    Update Balanza.java

--- Ejercicio4/Balanza.java
+++ Ejercicio4/Balanza.java
@@ -1,14 +1,29 @@
 package ar.edu.unlp.info.oo1.ejercicio4;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Balanza {
 	private List<Producto> productos;
-	// Balanza deberia tener tambien la cantidad de prodcutos y el peso y precio total??
 	
 	public Balanza() {
+		this.productos = new ArrayList<Producto>();
 		this.ponerEnCero();
+	}
+
+	public int getCantidadDeProductos() {
+		return this.productos.size();
+	}
+	
+	public double getPrecioTotal() {
+		return this.productos.stream()
+				.mapToDouble(prod -> prod.getPrecio())
+				.sum();
+	}
+	
+	public double getPesoTotal() {
+		return this.productos.stream()
+				.mapToDouble(prod -> prod.getPeso())
+				.sum();
 	}
 
 	public void ponerEnCero() {
