37dfd01fc499744e87ea50f07be0381ad0cfde85
User <User@DESKTOP-F5G59M4>
2024-09-20 10:09:12 -0300

    Corregí ejercicio 6

--- Ejercicio6/ejercicio6-Genealogia/src/main/java/ar/edu/unlp/info/oo1/ejercicio6/Mamifero.java
+++ Ejercicio6/ejercicio6-Genealogia/src/main/java/ar/edu/unlp/info/oo1/ejercicio6/Mamifero.java
@@ -43,7 +43,7 @@ public class Mamifero {
 	}
 	
 	// PADRES --------------------------
-	public boolean tienePadre() { // es necesario?
+	public boolean tienePadre() {
 		return this.padre != null;
 	}
 
@@ -55,7 +55,7 @@ public class Mamifero {
 		this.padre = padre;
 	}
 
-	public boolean tieneMadre() { // es necesario?
+	public boolean tieneMadre() {
 		return this.madre != null;
 	}
 
@@ -67,94 +67,108 @@ public class Mamifero {
 		this.madre = madre;
 	}
 
-	// ABUELOS --------------------------  estan mal
+	// ABUELOS -------------------------- 
 	public Mamifero getAbueloMaterno() {
-		return this.madre.getPadre();
+		if(this.tieneMadre()) {
+			return this.madre.getPadre();
+		}
+		else return null;
 	}
 	
 	public Mamifero getAbuelaMaterna() {
-		return this.madre.getMadre();
+		if(this.tieneMadre()) {
+			return this.madre.getMadre();
+		}
+		else return null;
 	}
 	
 	
 	public Mamifero getAbueloPaterno() {
-		return this.padre.getPadre();
+		if(this.tienePadre()) {
+			return this.padre.getPadre();			
+		}
+		else return null;
 	}
 	
 	public Mamifero getAbuelaPaterna() {
-		return this.padre.getMadre();
+		if(this.tienePadre()) {
+			return this.padre.getMadre();			
+		}
+		else return null;
 	}
 	
-	// ANCESTROS  -------------------------- esta mal
+	// ANCESTROS  --------------------------
+
+	
+	// Método principal que busca si un mamífero tiene como ancestro a otro
 	public boolean tieneComoAncestroA(Mamifero unMamifero) {
-	    // Verifica si el mamifero actual es el mismo que el traido por parametro
+	    // Si soy el mismo mamífero, devuelve false
 	    if (this.equals(unMamifero)) {
-	        return true;
+	        return false; // No puedo ser mi propio ancestro
 	    }
-
-	    // Verifica por el padre y sus ancestros
-	    if (this.padre != null && this.padre.tieneComoAncestroA(unMamifero)) {
-	        return true;
-	    }
-
-	    // Verifica por la madre y sus ancestros
-	    if (this.madre != null && this.madre.tieneComoAncestroA(unMamifero)) {
-	        return true;
-	    }
-
-	    // El ancestro no existe (esta bien asumir que puede no existir? dado que la conigna no me asegura que exista)
-	    return false;
+	    
+	    // Llama al método auxiliar para verificar en padres
+	    return tieneComoAncestroA(this.getPadre(), unMamifero) || tieneComoAncestroA(this.getMadre(), unMamifero);
 	}
 
-	// -----lu ------
+	// Método auxiliar recursivo
+	private boolean tieneComoAncestroA(Mamifero actual, Mamifero unMamifero) {
+	    // Si el actual es null, devuelve false (no hay ancestro)
+	    if (actual == null) {
+	        return false;
+	    }
+
+	    // Verifica si el actual es el mamífero buscado
+	    if (actual.equals(unMamifero)) {
+	        return true; // Encontrado
+	    }
+
+	    // Busca recursivamente en los padres
+	    return (actual.tienePadre() && tieneComoAncestroA(actual.getPadre(), unMamifero)) ||
+	           (actual.tieneMadre() && tieneComoAncestroA(actual.getMadre(), unMamifero));
+	}
 
 	
-	public Mamifero getAbueloPaterno() {
-		if (this.getPadre() != null){
-			return this.getPadre().getPadre();
-		}else {
-			return null;
+	
+	
+	/*
+	
+	OPCION 2: ME TIRA 2 FAILURES, NO ENTIENDO POR QUÉ. CONSULTAR QUÉ ES LO QUE FALLA
+	
+	
+	public boolean tieneComoAncestroA(Mamifero unMamifero) {		
+		// Si this es el Mamifero buscado, devuelve false (no puedo ser mi propio ancestro)
+		if (this.equals(unMamifero)) {
+			return false;
 		}
+		// Si this no es el Mamifero buscado,
+		// Llamo al método auxiliar recursivo para verificar en los padres
+	    return this.tienePadre() && this.getPadre().tieneComoAncestroA(unMamifero) ||
+	            this.tieneMadre() && this.getMadre().tieneComoAncestroA(unMamifero);
 	}
 	
-	public Mamifero getAbuelaPaterna() {
-		if (this.getPadre() != null) {
-			return this.getPadre().getMadre();
-		}else {
-			return null;
-		}	
-	}
-	
-	public Mamifero getAbueloMaterno() {
-		if (this.getMadre() != null) {
-			return this.getMadre().getPadre();
-		}else {
-			return null;
-		}
-		
-	}
-	
-	public Mamifero getAbuelaMaterna() {
-		if (this.getMadre() != null) {
-			return this.getMadre().getMadre();
-		}else {
-			return null;
-		}
-		
-	}
-	
-	public boolean tieneComoAncestroA(Mamifero unMamifero) {
-	    //pregunto al padre si es el ancestro, o si tiene ese ancestro
-	    if (this.getPadre() != null && (this.getPadre().equals(unMamifero) || 
-	    		this.getPadre().tieneComoAncestroA(unMamifero))) {
+	// Método auxiliar recursivo
+	public boolean tieneComoAncestroA(Mamifero actual, Mamifero unMamifero) {
+	    // Verifica si el actual es el mismo que el traído por parámetro
+	    if (actual.equals(unMamifero)) {
 	        return true;
 	    }
-	    //pregunto a mi madre si es el ancestro, o si tiene ese ancestro
-	    if (this.getMadre() != null && (this.getMadre().equals(unMamifero) || 
-	    		this.getMadre().tieneComoAncestroA(unMamifero))) {
-	        return true;
+
+	    // Si no lo es, busca recursivamente en sus padres
+	    boolean encontradoEnPadre = false;
+	    boolean encontradoEnMadre = false;
+
+	    if (actual.tienePadre()) {
+	        encontradoEnPadre = actual.tieneComoAncestroA(actual.getPadre(), unMamifero);
 	    }
-	    //retorno false en caso de que no se haya podido ingresar en los ifs
-	    return false;
+
+	    if (!encontradoEnPadre && actual.tieneMadre()) {
+	        encontradoEnMadre = actual.tieneComoAncestroA(actual.getMadre(), unMamifero);
+	    }
+
+	    return encontradoEnPadre || encontradoEnMadre;
 	}
+
+*/
+
 }
